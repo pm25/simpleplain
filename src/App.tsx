868d902b2cf01a2305c3622e099b1cb1c8f740cd
py <pyhuang97@gmail.com>
2024-12-21 17:46:31 +0800

    add more pages.

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,10 @@ import { HashRouter as Router, Routes, Route } from "react-router";
 import Layout from "@/layout";
 import Home from "@/pages/home";
 import About from "@/pages/about";
+import Movie from "@/pages/movie";
+import Music from "@/pages/music";
+import Project from "@/pages/project";
+import Article from "@/pages/article";
 
 export default function App() {
     return (
@@ -11,6 +15,10 @@ export default function App() {
                 <Route path="/" element={<Layout />}>
                     <Route index element={<Home />} />
                     <Route path="about" element={<About />} />
+                    <Route path="movie" element={<Movie />} />
+                    <Route path="music" element={<Music />} />
+                    <Route path="project" element={<Project />} />
+                    <Route path="article" element={<Article />} />
                 </Route>
             </Routes>
         </Router>
---
+++ src/pages/music/index.tsx
@@ -0,0 +1,13 @@
+export default function Music() {
+    return (
+        <div>
+            <span className="text-2xl">Music</span>
+            <div className="grid auto-rows-min gap-4 md:grid-cols-3">
+                <div className="aspect-video rounded-xl bg-muted/50" />
+                <div className="aspect-video rounded-xl bg-muted/50" />
+                <div className="aspect-video rounded-xl bg-muted/50" />
+            </div>
+            {/* <div className="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min" /> */}
+        </div>
+    );
+}
