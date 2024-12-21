f1af53b8e66beaa85975088e4d32f8f224ea3ac9
py <pyhuang97@gmail.com>
2024-12-21 17:11:13 +0800

    dynamically set the basename.

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,13 @@ import Home from "@/pages/home/page";
 import About from "@/pages/about/page";
 
 export default function App() {
+    const basename =
+        process.env.REACT_APP_BASENAME || window.location.pathname.split("/")[1]
+            ? `/${window.location.pathname.split("/")[1]}`
+            : "/";
+
     return (
-        <Router>
+        <Router basename={basename}>
             <Routes>
                 <Route path="/" element={<Layout />}>
                     <Route index element={<Home />} />
