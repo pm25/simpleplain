0a45130dab2654b74531187fa1adb1ea0d5dc4cb
py <pyhuang97@gmail.com>
2024-12-21 17:14:40 +0800

    remove update-env.js and uses hashRouter instead.

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,12 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router";
+import { HashRouter as Router, Routes, Route } from "react-router";
 
 import Layout from "@/layout";
 import Home from "@/pages/home/page";
 import About from "@/pages/about/page";
 
 export default function App() {
-    const basename =
-        process.env.REACT_APP_BASENAME || window.location.pathname.split("/")[1]
-            ? `/${window.location.pathname.split("/")[1]}`
-            : "/";
-
     return (
-        <Router basename={basename}>
+        <Router>
             <Routes>
                 <Route path="/" element={<Layout />}>
                     <Route index element={<Home />} />
--- update-env.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-const packageJson = require('./package.json');
-
-const homepage = packageJson.homepage || '/';
-
-// Write to .env file
-fs.appendFileSync(path.join(__dirname, '.env'), `REACT_APP_HOMEPAGE=${homepage}\n`);
